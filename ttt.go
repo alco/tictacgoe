beaab134ea6d5382b253d10aa2e65134e9d28162
Alexei Sholik <alcosholik@gmail.com>
2013-04-26 20:26:11 +0300

    Move parsing

--- ttt.go
+++ ttt.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type Board struct {
@@ -49,6 +51,47 @@ func getMove() (str string, err error) {
 	return str[:len(str)-1], err
 }
 
+func parseLetter(str string, coord *int) bool {
+	switch str {
+	case "a": *coord = 0
+	case "b": *coord = 1
+	case "c": *coord = 2
+	default:  return false
+	}
+	return true
+}
+
+func parseDigit(str string, coord *int) bool {
+	if len(str) == 1 && str[0] >= '1' && str[0] <= '3' {
+		*coord = int(str[0] - '1')
+		return true
+	}
+	return false
+}
+
+func parseMove(move string) (coords [2]int, err error) {
+	err = errors.New("err: Invalid move.")
+
+	if len(move) == 0 {
+		err = errors.New("err: Please make a move.")
+		return
+	}
+
+	var fields = strings.Fields(move)
+	if len(fields) != 2 {
+		return
+	}
+
+	if !(parseLetter(fields[0], &coords[0]) || parseLetter(fields[1], &coords[0])) {
+		return
+	}
+	if !(parseDigit(fields[0], &coords[1]) || parseDigit(fields[1], &coords[1])) {
+		return
+	}
+
+	return coords, nil
+}
+
 func main() {
 	fmt.Println("*** Welcome to Tic-Tac-Go ***")
 	var board = NewBoard()
@@ -62,8 +105,23 @@ func main() {
 			if err != nil {
 				os.Exit(0)
 			}
-			board.b[0][0] = 'X'
-			println(move)
+
+			coords, err := parseMove(move)
+			if err != nil {
+				fmt.Printf("\x1b[31m%v\x1b[0m\n", err)
+				board.draw()
+				continue
+			}
+
+			var cell = &board.b[coords[0]][coords[1]]
+			if *cell == ' ' {
+				*cell = 'X'
+			} else {
+				fmt.Println("\x1b[31mCell already taken.\x1b[0m")
+				board.draw()
+				continue
+			}
+
 			break
 		}
 	}
