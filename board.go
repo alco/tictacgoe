8fe2e3185e8659eb0ad9f42fcd99ad6b8163a64a
Alexei Sholik <alcosholik@gmail.com>
2013-04-27 11:21:51 +0300

    Proper winning and losing messages

---
+++ board.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ownChar, oppChar int
+
+const (
+	NoMove = iota
+	OKMove
+
+	GameFinished
+	MeWin
+	HeWin
+	Draw
+)
+
+func formatCell(char int) string {
+	var seq string
+	if char == oppChar {
+		seq = "\x1b[41m[%c]\x1b[0m"
+	} else if char == ownChar {
+		seq = "\x1b[7m[%c]\x1b[0m"
+	} else {
+		seq = "[%c]"
+	}
+	return fmt.Sprintf(seq, char)
+}
+
+func mapChar(char int) int {
+	if char == ownChar {
+		return MeWin
+	}
+	return HeWin
+}
+
+type Board struct {
+	b         [3][3]int
+	freeCells int
+}
+
+func NewBoard() *Board {
+	var b = Board{}
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			b.b[i][j] = ' '
+		}
+	}
+	b.freeCells = 9
+	return &b
+}
+
+func (b *Board) draw() {
+	fmt.Printf("\n   1   2   3\na %s %s %s\nb %s %s %s\nc %s %s %s\n",
+		formatCell(b.b[0][0]), formatCell(b.b[0][1]), formatCell(b.b[0][2]),
+		formatCell(b.b[1][0]), formatCell(b.b[1][1]), formatCell(b.b[1][2]),
+		formatCell(b.b[2][0]), formatCell(b.b[2][1]), formatCell(b.b[2][2]))
+
+}
+
+func (b *Board) checkWinningCondition(coords [2]int) int {
+	var i, j = coords[0], coords[1]
+	var char int
+
+	// Check horizontal
+	char = b.b[i][0]
+	if char != ' ' && b.b[i][1] == char && b.b[i][2] == char {
+		return mapChar(char)
+	}
+
+	// Check vertical
+	char = b.b[0][j]
+	if char != ' ' && b.b[1][j] == char && b.b[2][j] == char {
+		return mapChar(char)
+	}
+
+	// Check diagonals
+	if (i+j)%2 == 1 {
+		return 0 // not a diagonal
+	}
+
+	char = b.b[0][0]
+	if char != ' ' && b.b[1][1] == char && b.b[2][2] == char {
+		return mapChar(char)
+	}
+
+	char = b.b[0][2]
+	if char != ' ' && b.b[1][1] == char && b.b[2][0] == char {
+		return mapChar(char)
+	}
+
+	return 0
+}
+
+func (b *Board) makeMove(coords [2]int, char int) (int, error) {
+	var cell = &b.b[coords[0]][coords[1]]
+	if *cell == ' ' {
+		*cell = char
+		b.freeCells -= 1
+		if result := b.checkWinningCondition(coords); result != 0 {
+			return result, nil
+		} else if b.freeCells == 0 {
+			return Draw, nil
+		}
+		return OKMove, nil
+	}
+	return NoMove, errors.New("Cell already taken.")
+}
+
+func (b *Board) waitForOpponent() (int, error) {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if b.b[i][j] == ' ' {
+				return b.makeMove([2]int{i, j}, 'O')
+			}
+		}
+	}
+	return NoMove, errors.New("No free cell found")
+}
--- ttt.go
+++ ttt.go
@@ -8,117 +8,6 @@ import (
 	"strings"
 )
 
-func formatCell(char int) string {
-	var seq string
-	if char == 'O' {
-		seq = "\x1b[41m[%c]\x1b[0m"
-	} else if char == 'X' {
-		seq = "\x1b[7m[%c]\x1b[0m"
-	} else {
-		seq = "[%c]"
-	}
-	return fmt.Sprintf(seq, char)
-}
-
-type Board struct {
-	b         [3][3]int
-	freeCells int
-}
-
-func NewBoard() *Board {
-	var b = Board{}
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			b.b[i][j] = ' '
-		}
-	}
-	b.freeCells = 9
-	return &b
-}
-
-func (b *Board) draw() {
-	fmt.Printf(`
-   1   2   3
-a %s %s %s
-b %s %s %s
-c %s %s %s
-`,
-		formatCell(b.b[0][0]), formatCell(b.b[0][1]), formatCell(b.b[0][2]),
-		formatCell(b.b[1][0]), formatCell(b.b[1][1]), formatCell(b.b[1][2]),
-		formatCell(b.b[2][0]), formatCell(b.b[2][1]), formatCell(b.b[2][2]))
-
-}
-
-func (b *Board) checkWinningCondition(coords [2]int) int {
-	var i, j = coords[0], coords[1]
-	var char int
-
-	// Check horizontal
-	char = b.b[i][0]
-	if char != ' ' && b.b[i][1] == char && b.b[i][2] == char {
-		return MeWin
-	}
-
-	// Check vertical
-	char = b.b[0][j]
-	if char != ' ' && b.b[1][j] == char && b.b[2][j] == char {
-		return MeWin
-	}
-
-	// Check diagonals
-	if (i+j)%2 == 1 {
-		return 0 // not a diagonal
-	}
-
-	char = b.b[0][0]
-	if char != ' ' && b.b[1][1] == char && b.b[2][2] == char {
-		return MeWin
-	}
-
-	char = b.b[0][2]
-	if char != ' ' && b.b[1][1] == char && b.b[2][0] == char {
-		return MeWin
-	}
-
-	return 0
-}
-
-const (
-	OKMove = iota
-	NoMove
-	GameFinished
-
-	MeWin
-	HeWin
-	Draw
-)
-
-func (b *Board) makeMove(coords [2]int, char int) (int, error) {
-	var cell = &b.b[coords[0]][coords[1]]
-	if *cell == ' ' {
-		*cell = char
-		b.freeCells -= 1
-		if result := b.checkWinningCondition(coords); result != 0 {
-			return result, nil
-		} else if b.freeCells == 0 {
-			return Draw, nil
-		}
-		return OKMove, nil
-	}
-	return NoMove, errors.New("Cell already taken.")
-}
-
-func (b *Board) waitForOpponent() (int, error) {
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 3; j++ {
-			if b.b[i][j] == ' ' {
-				return b.makeMove([2]int{i, j}, 'O')
-			}
-		}
-	}
-	return NoMove, errors.New("No free cell found")
-}
-
 var stdin = bufio.NewReader(os.Stdin)
 
 func getMove() (str string, err error) {
@@ -132,17 +21,11 @@ func getMove() (str string, err error) {
 }
 
 func parseLetter(char byte, coord *int) bool {
-	switch char {
-	case 'a':
-		*coord = 0
-	case 'b':
-		*coord = 1
-	case 'c':
-		*coord = 2
-	default:
-		return false
+	if char >= 'a' && char <= 'c' {
+		*coord = int(char - 'a')
+		return true
 	}
-	return true
+	return false
 }
 
 func parseDigit(char byte, coord *int) bool {
@@ -181,6 +64,7 @@ func printError(err error) {
 func main() {
 	fmt.Println("*** Welcome to Tic-Tac-Go ***")
 	var board = NewBoard()
+	ownChar, oppChar = 'X', 'O'
 
 	var checkResult = func(result int) {
 		if result > GameFinished {
